a3e5b2de105456c8cdae096695e8e6a0a7ad1314
Mubashshir Mohammed <39416@corp.hexaware.com>
2017-12-11 14:55:43 +0530

    Test Draft

--- index.js
+++ index.js
@@ -31,10 +31,23 @@ alexaApp.express({
 });
 
 alexaApp.launch(function (request, response) {
+    let card = {
+        "card": {
+            "type": "Standard",
+            "title": "This is the Greeting message from HDFC Assistant",
+            "text": "Your ride is on the way to 123 Main Street!\nEstimated cost for this ride: $25",
+            "image": {
+              "smallImageUrl": "https://carfu.com/resources/card-images/race-car-small.png",
+              "largeImageUrl": "https://carfu.com/resources/card-images/race-car-large.png"
+            }
+          }
+      }
     console.log(JSON.stringify(request));
-    response.say("HELLO THERE. I AM AN HDFC ASSISTANT. YOU CAN ASK ME DETAILS ABOUT AN HDFC EMPLOYEE, YOUR PENDING SERVICE REQUESTS OR MAKING A NEW SERVICE REQUEST.!")
-        .reprompt("You there?");
-});
+    // response.say("HELLO THERE. I AM AN HDFC ASSISTANT. YOU CAN ASK ME DETAILS ABOUT AN HDFC EMPLOYEE, YOUR PENDING SERVICE REQUESTS OR MAKING A NEW SERVICE REQUEST.!")
+    //     .reprompt("You there?");
+
+        response.card(card);
+    });
 
 //   alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
 
@@ -52,8 +65,9 @@ alexaApp.intent("employeedetailsIntent",
 alexaApp.intent("welcomeIntent",
     function (request, response) {
         console.log(JSON.stringify(request));
-        response.say("HELLO THERE. I AM AN HDFC ASSISTANT. YOU CAN ASK ME DETAILS ABOUT AN HDFC EMPLOYEE, YOUR PENDING SERVICE REQUESTS OR MAKING A NEW SERVICE REQUEST.!")
-            .reprompt("You there?");
+        // response.say("HELLO THERE. I AM AN HDFC ASSISTANT. YOU CAN ASK ME DETAILS ABOUT AN HDFC EMPLOYEE, YOUR PENDING SERVICE REQUESTS OR MAKING A NEW SERVICE REQUEST.!")
+        //     .reprompt("You there?");
+        response.card(card);
     }
 );
 
